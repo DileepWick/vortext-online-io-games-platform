f60ec6181bae8cdff7e5864389216b146192d8dc
TharinduAri <tharindulakshita2001@gmail.com>
2024-08-19 20:51:58 +0530

    gsdgs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 //Pages
-import CreateBook from "./pages/CreateBook";
 import Home from "./pages/Home";
 import Login from "./pages/login";
-import ManageBooks from "./pages/ManageBooks";
 import Profile from "./pages/profile";
 import AddGameForm from "./pages/add_new_game";
 import Shop from "./pages/Shop";
@@ -16,8 +14,6 @@ import ArticleList from "./pages/articles";
 import Contact from "./pages/Contact";
 import GamingSessions from "./pages/GamingSessions";
 import SessionHistory from "./pages/SessionHistory";
-import HandleRentals from "./pages/HandleRentals";
-
 
 //Gameplay
 import GameEmbed from "./pages/GameEmbed";
@@ -31,13 +27,12 @@ import Review_manager from "../dashboards/review_dashboard";
 import ContactDash from "../dashboards/Customer suport";
 import SessionManagerDash from "../dashboards/session_manager_dashbord";
 import StaffManager from "../dashboards/staff_manager_dashboard";
+import UserManagementDashboard from "../dashboards/UserManagementDashboard";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/books/addNewBook" element={<CreateBook />} />
-      <Route path="/books/manageBooks" element={<ManageBooks />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/adminAddNewGame" element={<AddGameForm />} />
@@ -58,15 +53,10 @@ const App = () => {
       <Route path="/staffManager" element={<StaffManager />} />
       <Route path="/review_dashboard" element={<Review_manager />} />
       <Route path="/playgame/:src/:title" element={<GameEmbed />} />
-<<<<<<< Updated upstream
-=======
 
       <Route path="/UserManagementDashboard" element={<UserManagementDashboard />} />
-      <Route path="/handleRentals/:stockid" element={<HandleRentals />} />
 
->>>>>>> Stashed changes
       <Route path="/SessionHistory" element={<SessionHistory />} />
-      <Route path="/handleRentals/:stockid" element={<HandleRentals />} />
 
     </Routes>
   );
