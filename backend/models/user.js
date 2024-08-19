7ac10687384584930b45c9d9e9bc4b5dc47f2030
dasundu <dasunathauda99@gmail.com>
2024-08-19 21:52:12 +0530

    User management
    1.Login & signup
    2.User manage panel

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,40 +1,41 @@
+// user.js
+
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    profilePic: {
-        type: String,
-        default: "https://res.cloudinary.com/dhcawltsr/image/upload/v1719572309/user_swzm7h.webp"
-    },
-    birthday: {
-        type: Date,
-        required: true
-    },
-    age: {
-        type: Number,
-        required: true
-    },
-    playerType: {
-        type: String,
-        enum: ['Kid', 'Teenager', 'Adult'],
-        required: true
-    }
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  profilePic: {
+    type: String,
+    default: "https://res.cloudinary.com/dhcawltsr/image/upload/v1719572309/user_swzm7h.webp",
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  playerType: {
+    type: String,
+    enum: ["Kid", "Teenager", "Adult"],
+  },
 });
 
 export const User = mongoose.model("User", UserSchema);
