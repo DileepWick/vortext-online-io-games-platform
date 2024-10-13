2cc51085e0b40e72315189ceea6c263bb2cecad8
Kavi-Gomez <thuspura@gmail.com>
2024-10-14 02:56:24 +0530

    make beautiful
    make beautiful

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,4 +1,4 @@
-// message.js
+// message.js (Update the Message model)
 import mongoose from 'mongoose';
 
 const messageSchema = new mongoose.Schema({
@@ -6,6 +6,7 @@ const messageSchema = new mongoose.Schema({
   messageUser: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false }, // Add this field
 });
 
 const Message = mongoose.model('Message', messageSchema);
